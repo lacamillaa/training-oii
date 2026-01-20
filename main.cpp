ecdee531114e9c4ea65d4e8ea4173257d9ee59ce
lacamillaa <12999356@itisrossi.vi.it>
2026-01-20 22:47:08 +0100

    costruttori

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 int main() {
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
-
-    for (int i = 1; i <= 5; i++) {
-        std::cout << "i = " << i << std::endl;
-    }
-
+    vector<int> v1; // crea un array vuoto
+    vector<int> v2(5); // crea un array di 5 elementi tutti a 0
+    vector<int> v3(5, -1); // crea un array di 5 elementi tutti a -1
+    vector<int> v4(v2); // crea una copia di v2
     return 0;
 }
