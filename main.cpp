3475450b9542b513fd3b7f591fe4198d0dbf80b3
lacamillaa <12999356@itisrossi.vi.it>
2026-01-25 03:07:01 +0100

    costruttori

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
+#include <deque>
 #include <iostream>
 
+using namespace std;
+
 int main() {
+    deque<int> q1;
+    deque<int> q2(5);
+    deque<int> q3(5, 3);
+    deque<int> q4(q2);
+
     return 0;
 }
