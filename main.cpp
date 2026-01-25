6ad25756a2ed3dfc96d414562948e6701ef2df59
lacamillaa <12999356@itisrossi.vi.it>
2026-01-25 03:20:22 +0100

    metodi push e pop

--- main.cpp
+++ main.cpp
@@ -14,6 +14,12 @@ int main() {
     q2.insert(q2.begin(), 10, 8); // inserisce 8 per 10 volte
     q2.insert(q2.end(), {1, 2, 3, 4, 5, 6, 7, 8, 9}); // inserisce la lista alla fine
 
+    q2.pop_back(); // rimuove l'ultimo elemento
+    q2.pop_front(); // rimuove il primo elemento
+
+    q2.push_back(-1); // aggiunge alla fine
+    q2.push_front(-1); // aggiunge all'inizio
+
     for (auto i : q2) {
         cout << i << " ";
     }
