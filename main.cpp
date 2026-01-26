6a63f16e9af2a30d563744b82aadbb464295cc14
lacamillaa <12999356@itisrossi.vi.it>
2026-01-26 22:18:11 +0100

    metodo insert()

--- main.cpp
+++ main.cpp
@@ -29,5 +29,17 @@ int main() {
 
     cout << map.size() << endl; // numero di coppie key-value
 
+    // restituisce una pair<iterator, bool> p
+    // p.first -> puntatore alla pair inserita
+    // p.second -> bool (esito dell'inserimento)
+    auto rs1 = map.insert(pair<char, int>('a', 'a'));
+    if (rs1.second) {
+        cout << rs1.first->first << " è stato inserito" << endl;
+    }
+    auto rs2 = map.insert(pair<char, int>('B', 'B'));
+    if (!rs2.second) {
+        cout << rs2.first->first << " NON è stato inserito" << endl;
+    }
+
     return 0;
 }
