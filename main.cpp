2ee6be62b3320e4cb53e1fe6108ac1a69b61a336
lacamillaa <12999356@itisrossi.vi.it>
2026-01-26 21:14:37 +0100

    costrutttori

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
+#include <algorithm>
 #include <iostream>
+#include <set>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    set<int> s1;
+    set<int> s2({1, 2, 5, 4}); // mette in ordine in automatico il set
+    set<int> s3(s2);
+    ranges::copy(s2, ostream_iterator<int>(cout, " "));
     return 0;
 }
