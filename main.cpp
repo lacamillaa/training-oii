90666cae2f68be0288fad1610d23bf0164ce20ff
lacamillaa <12999356@itisrossi.vi.it>
2026-01-26 20:48:41 +0100

    puntatori, next e prev

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <list>
 
@@ -11,5 +12,16 @@ int main() {
 
     cout << l2.front() << " "; // restituisce il primo elemento
     cout << l2.back() << endl; // restituisce l'ultimo elemento
+
+    ranges::copy(l3, ostream_iterator<int>(cout, " "));
+    cout << endl;
+    cout << *prev(l3.end()) << " ";
+    cout << *next(l3.begin()) << endl;
+
+    l3.assign({ 1, 4, 2, 7, 2 });
+    ranges::copy(l3, ostream_iterator<int>(cout, " "));
+    cout << endl;
+    cout << *l3.rbegin() << " ";
+    cout << *prev(l3.rend()) << endl;
     return 0;
 }
