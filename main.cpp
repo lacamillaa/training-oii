75beed2ff2e0a045e7882b961287ccfef3177de8
lacamillaa <12999356@itisrossi.vi.it>
2026-01-26 10:24:54 +0100

    assign

--- main.cpp
+++ main.cpp
@@ -10,5 +10,12 @@ int main() {
     forward_list<int> fl3(7, -3); // 7 elementi inizializzati a -3
     forward_list<int> fl4(fl2); // copia di fl2
     ranges::copy(fl4, ostream_iterator<int>(cout, " "));
+    cout << endl;
+
+    fl1.assign(5, -2); // 5 elementi assegnati a -2
+    fl1.assign({1, 2, 3, 4, 5}); // copia la lista in fl1
+    fl2.assign(fl1.begin(), fl1.end()); // copia fl1 in fl2
+    ranges::copy(fl2, ostream_iterator<int>(cout, " "));
+    cout << endl;
     return 0;
 }
