9f5f2dc246c657c2e43debd8b47d9c81e5757e87
lacamillaa <12999356@itisrossi.vi.it>
2026-01-26 21:22:32 +0100

    metodo insert

--- main.cpp
+++ main.cpp
@@ -16,5 +16,23 @@ int main() {
     cout << *s2.rbegin() << " ";
     cout << *s2.rend() << " ";
     cout << endl;
+
+    // insert inserisce solo se l'elemento non è già presente
+    // result.second == true => l'elemento è stato inserito
+    // result.second == false => l'elemento NON è stato inserito
+    auto result = s2.insert(5);
+    if (result.second) {
+        cout << *result.first << " è stato inserito" << endl;
+    }
+    else {
+        cout << *result.first << " non è stato inserito" << endl;
+    }
+    auto result2 = s2.insert(7);
+    if (result2.second) {
+        cout << *result2.first << " è stato inserito" << endl;
+    }
+    else {
+        cout << *result2.first << " non è stato inserito" << endl;
+    }
     return 0;
 }
