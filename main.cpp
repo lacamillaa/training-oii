51f693f0d1626f2ed1531b8b0127b51d6ae11895
lacamillaa <12999356@itisrossi.vi.it>
2026-01-26 22:10:38 +0100

    puntatori e metodo size()

--- main.cpp
+++ main.cpp
@@ -6,8 +6,9 @@ using namespace std;
 
 void print(const map<char, int>& m) { // il & evita che venga passata una copia di m alla funzione
     for (auto i : m) {
-        cout << i.first << " : " << i.second << endl;
+        cout << i.first << " : " << i.second << " - ";
     }
+    cout << endl;
 }
 
 int main() {
@@ -16,5 +17,17 @@ int main() {
         map[i] = i; // l'operatore [] costruisce la chiave se non esiste
     }
     print(map);
+
+    for (int i = 65; i < 91; i++) {
+        cout << map[i] << endl;
+    }
+
+    auto it = map.begin();
+    cout << it->first << " : " << it->second << endl;
+    auto itr = map.rbegin();
+    cout << (*itr).first << " : " << (*itr).second << endl;
+
+    cout << map.size() << endl; // numero di coppie key-value
+
     return 0;
 }
