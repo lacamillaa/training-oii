8acccd10828798cb1477a22b661012142ef99562
lacamillaa <12999356@itisrossi.vi.it>
2026-01-26 21:50:29 +0100

    costruttore, stampa e operatore &

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
+#include <algorithm>
 #include <iostream>
+#include <map>
+
+using namespace std;
+
+void print(const map<char, int>& m) { // il & evita che venga passata una copia di m alla funzione
+    for (auto i : m) {
+        cout << i.first << " : " << i.second << endl;
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    map<char, int> map;
+    for (int i = 65; i < 91; i++) {
+        map[i] = i; // l'operatore [] costruisce la chiave se non esiste
+    }
+    print(map);
     return 0;
 }
