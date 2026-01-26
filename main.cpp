f218d47c81f2252649551ae1288b3b2c30ad25f7
lacamillaa <12999356@itisrossi.vi.it>
2026-01-26 20:43:28 +0100

    front e back

--- main.cpp
+++ main.cpp
@@ -8,5 +8,8 @@ int main() {
     list<int> l2(7);
     list<int> l3(6, 4);
     list<int> l4(l2);
+
+    cout << l2.front() << " "; // restituisce il primo elemento
+    cout << l2.back() << endl; // restituisce l'ultimo elemento
     return 0;
 }
