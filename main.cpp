b007d463728ba183ff1c592e48cfc3df5c96cea1
lacamillaa <12999356@itisrossi.vi.it>
2026-01-26 20:41:38 +0100

    costruttori

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <list>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    list<int> l1;
+    list<int> l2(7);
+    list<int> l3(6, 4);
+    list<int> l4(l2);
     return 0;
 }
