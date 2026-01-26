422e6a3cbc0fc014d88227bae947db8db17b24bd
lacamillaa <12999356@itisrossi.vi.it>
2026-01-26 21:16:43 +0100

    puntatori

--- main.cpp
+++ main.cpp
@@ -9,5 +9,12 @@ int main() {
     set<int> s2({1, 2, 5, 4}); // mette in ordine in automatico il set
     set<int> s3(s2);
     ranges::copy(s2, ostream_iterator<int>(cout, " "));
+    cout << endl;
+
+    cout << *s2.begin() << " ";
+    cout << *s2.end() << " ";
+    cout << *s2.rbegin() << " ";
+    cout << *s2.rend() << " ";
+    cout << endl;
     return 0;
 }
