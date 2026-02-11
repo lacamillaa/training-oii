e3390c69ea9ee5fd9ce6a946d22da82025268732
lacamillaa <12999356@itisrossi.vi.it>
2026-02-11 10:10:42 +0100

    desserts tentativo 4 (100/100)

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 #include <algorithm>
 #include <fstream>
 #include <iostream>
-#include <map>
 #include <string>
 #include <vector>
 
@@ -14,24 +13,24 @@ int main() {
     ifstream cin("input.txt");
     ofstream cout("output.txt");
 
-    map<int, int> m;
-    // coppie <dolcezza, conteggio> (frutto = P)
-
     int N, P;
     cin >> N >> P;
 
     vector<int> S(N);
-    for (int i = 0; i < N; ++i) {
+    for (int i = 0; i < N; ++i)
         cin >> S[i];
-    }
+
+    vector<int> S1;
 
     vector<int> F(N);
     for (int i = 0; i < N; ++i) {
         cin >> F[i];
         if (F[i] == P) {
-            m[S[i]]++;
+            S1.push_back(S[i]);
         }
     }
+
+    sort(S1.begin(), S1.end());
 
     int Q;
     cin >> Q;
@@ -41,16 +40,13 @@ int main() {
     vector<int> l(Q), r(Q), k(Q);
     for (int i = 0; i < Q; ++i) {
         cin >> l[i] >> r[i] >> k[i];
-        auto ptrS = m.lower_bound(l[i]);
-        auto ptrF = m.upper_bound(r[i]);
-        long long count = 0;
-        while (ptrS != ptrF) {
-            count += ptrS->second;
-            ++ptrS;
-        }
-        if (count >= k[i]) answer[i] = "YES";
+        auto ptrS = lower_bound(S1.begin(), S1.end(), l[i]);
+        auto ptrF = upper_bound(S1.begin(), S1.end(), r[i]);
+        int diff = ptrF - ptrS;
+        if (diff >= k[i]) answer[i] = "YES";
         else answer[i] = "NO";
     }
+
 
 
     for (int i = 0; i < Q; ++i)
