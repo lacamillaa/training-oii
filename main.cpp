2336d967439194b80ee6edd9e4feca045b384231
lacamillaa <12999356@itisrossi.vi.it>
2026-03-05 16:37:10 +0100

    ropes tentativo 1 (40/100)

---
+++ main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+int find_end_node(int start, vector<int>& end) {
+    while (end[start] != start) {
+        start = end[start];
+    }
+    return start;
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int N;
+    cin >> N;
+
+    vector<int> P(N);
+    for (int i = 0; i < N; i++) {
+        cin >> P[i];
+    }
+    vector<int> T(N);
+    for (int i = 0; i < N; i++) {
+        cin >> T[i];
+    }
+
+    vector<vector<int>> children(N + 1);
+    for (int i = 0; i < N; i++) {
+        int p = P[i];
+        children[p].push_back(i + 1);
+    }
+
+    vector<int> end(N + 1);
+    stack<int> v;
+    v.push(0);
+    while (!v.empty()) {
+        int n = v.top();
+        v.pop();
+        int max = 0;
+        int maxN = n;
+        for (auto c : children[n]) {
+            v.push(c);
+            if (T[c - 1] > max) {
+                max = T[c - 1];
+                maxN = c;
+            }
+        }
+        end[n] = maxN;
+    }
+
+    int root = 0;
+    int time = 0;
+    while (end[root] != root) {
+        int new_parent = end[root];
+        int end_node = find_end_node(new_parent, end);
+        auto it = children[root].begin();
+        while (it != children[root].end()) {
+            auto child = *it;
+            if (child != new_parent) {
+                time += T[child - 1];
+                P[child - 1] = end_node;
+                end[end_node] = child;
+                children[root].erase(it);
+                children[end_node].push_back(child);
+                end_node = find_end_node(end_node, end);
+            } else it++;
+        }
+        root = end[root];
+    }
+
+    cout << time << endl;
+
+    return 0;
+}
